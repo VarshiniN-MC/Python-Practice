f4b2d3c33c4e69042e9c46dd89cd572ccba59630
Varshini N <varshininageshh09@gmail.com>
2026-03-05 20:13:41 +0530

    Added palindrome program in strings folder

---
+++ strings/palindrome.py
@@ -0,0 +1,13 @@
+#String Palindrome
+def reverse():
+    rev=""
+    for i in str:
+        rev=i+rev
+    return rev
+str=input()
+rev= reverse()
+print(rev)
+if rev==str:
+    print("palindrome")
+else:
+    print("not palindrome")
