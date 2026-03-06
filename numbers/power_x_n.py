d8ac888da608ad3128b477f3514d306041f21bd9
Varshini N <varshininageshh09@gmail.com>
2026-03-06 19:38:33 +0530

    Added python program to calculate power of a number in numbers folder

---
+++ numbers/power_x_n.py
@@ -0,0 +1,6 @@
+#calculate power of a number
+import math
+number=int(input("Enter the number : "))
+power=int(input("Enter the power : "))
+x=math.pow(number,power)
+print("The value of ",number, "to the power ",power,"is ",x)
