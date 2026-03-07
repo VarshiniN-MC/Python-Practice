87c23c207bb91b716c70a67f7fcdfa17512120f0
Varshini N <varshininageshh09@gmail.com>
2026-03-07 13:02:09 +0530

    Removed unused directory

--- numbers/power_x_n.py
+++
@@ -1,6 +0,0 @@
-#calculate power of a number
-import math
-number=int(input("Enter the number : "))
-power=int(input("Enter the power : "))
-x=math.pow(number,power)
-print("The value of ",number, "to the power ",power,"is ",x)
