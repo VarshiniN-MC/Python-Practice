f7599ca7824a5d169df62439ae5a6160bd4e6bec
Varshini N <varshininageshh09@gmail.com>
2026-03-07 13:41:32 +0530

    Removed unused directory

--- strings/character_occurrence.py
+++
@@ -1,9 +0,0 @@
-#To check number of occurrence of each character in the string using dictionary
-str=input("Enter the words ")
-dictionary={}
-for i in str:
-    if i in dictionary:
-        dictionary[i]=dictionary[i]+1
-    else:
-       dictionary[i]=1
-print("The number of occurrences are : ",dictionary)
