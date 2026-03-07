e99a17ec85174aa9ff3c9299681045d8a25fdb8f
Varshini N <varshininageshh09@gmail.com>
2026-03-07 12:41:37 +0530

    Added matrix addition & subtraction program (using NumPy)

---
+++ matrix/matrix_add_sub_numpy.py
@@ -0,0 +1,8 @@
+#Matrix Addition and Subtraction using NumPy
+import numpy as np
+m1=np.array([[7, 5],[11, 13]])
+m2=np.array([[6, 28],[10, 3]])
+print("Addition of the two matrices:")
+print(np.add(m1,m2))
+print("Subtraction of the two matrices:")
+print(np.subtract(m1, m2))
